277963c94a3aa3af2ae0e72b2db1b438bd387213
Eliana Ḿendez <eliamargi@gmail.com>
2025-05-24 17:55:03 -0500

    se actualiza mensaje para finalizar aplicacion

--- src/Principal.java
+++ src/Principal.java
@@ -31,7 +31,7 @@ public class Principal {
             try {
                 int parUsuario = lectura.nextInt();
                 if (parUsuario == 7) {
-                    System.out.println("Finalizando la aplicación. ¡Gracias por usar nuestro conversor!");
+                    System.out.println("Finalizando la aplicación. ¡Gracias por usar nuestro conversor de monedas!");
                     break;
                 }
 
